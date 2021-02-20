67ac58d1f5e4b55de85e02445f1c00189a86b0e6
Lee Shiueh <lee.shiueh@gmail.com>
2021-02-20 09:07:33 +0800

    project init

---
+++ pytaiseia101/__version__.py
@@ -0,0 +1,10 @@
+__title__ = 'pytaiseia101'
+__description__ = """A Python library for smart home accessory device control protocol designed by Taiwan Smart 
+Energy Industry Association (TaiSEIA)."""
+__url__ = 'https://pytaiseia101.readthedocs.io'
+__version__ = '0.0.1'
+# __build__ = 0x022501
+__author__ = 'Lee Shiueh'
+__author_email__ = 'lee.shiueh@gmail.com'
+__license__ = 'Apache 2.0'
+# __copyright__ = 'Copyright 2021'
