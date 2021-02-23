de361bb27928cc413e23b3fc1e44e8cef177442b
Lee Shiueh <lee.shiueh@gmail.com>
2021-02-23 15:36:30 +0800

    revise module test script

--- py_tests.py
+++ py_tests.py
@@ -4,7 +4,7 @@ import unittest
 def test():
     tests = unittest.TestLoader().discover('./tests', pattern='test*.py')
     results = unittest.TextTestRunner(verbosity=2).run(tests)
-    return 0 if results.wasSuccessful() else 0
+    return 0 if results.wasSuccessful() else 1
 
 
 test()
