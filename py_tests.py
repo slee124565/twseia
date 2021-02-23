7e291485e7ae9ae34dbc8ce5da46c34ae4ee692d
Lee Shiueh <lee.shiueh@gmail.com>
2021-02-23 15:36:10 +0800

    revise module test script

--- py_tests.py
+++ py_tests.py
@@ -1,8 +1,10 @@
-import sys
 import unittest
 
 
-if __name__ == '__main__':
+def test():
     tests = unittest.TestLoader().discover('./tests', pattern='test*.py')
     results = unittest.TextTestRunner(verbosity=2).run(tests)
-    sys.exit(0 if results.wasSuccessful() else 0)
+    return 0 if results.wasSuccessful() else 0
+
+
+test()
