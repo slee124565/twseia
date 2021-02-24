7d3291adc57c2599d0fdedbfa7b5b32d5d681339
Lee Shiueh <lee.shiueh@gmail.com>
2021-02-24 14:39:00 +0800

    revise travis cfg

--- tests/test_smart_application.py
+++ tests/test_smart_application.py
@@ -9,12 +9,12 @@ sys.path.append('.')
 class TestSmartApplication(unittest.TestCase):
 
     def test_read_service(self):
-        # raise NotImplementedError
-        pass
+        raise NotImplementedError
+        # pass
 
     def test_write_service(self):
-        # raise NotImplementedError
-        pass
+        raise NotImplementedError
+        # pass
 
 
 if __name__ == '__main__':
