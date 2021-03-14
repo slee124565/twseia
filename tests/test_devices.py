8cbbf510931b08666c061702fcad2cba3338e347
Lee Shiueh <lee.shiueh@gmail.com>
2021-03-14 22:06:04 +0800

    mark devp testcase

--- tests/test_devices.py
+++ tests/test_devices.py
@@ -8,96 +8,96 @@ logging.getLogger('').handlers = []
 logging.basicConfig(level=logging.INFO, format=FORMAT)
 
 
-class TestSmartApplication(unittest.TestCase):
-
-    def test_create_register_cmd(self):
-        register = twseia.SmartApplication.create_read_register_cmd(service_id=0x00)
-        self.assertEqual(register, [0x06, 0x00, 0x00, 0xFF, 0xFF, 0x06])
-        register = twseia.SmartApplication.create_read_class_id_cmd(service_id=0x00)
-        self.assertEqual(register, [0x06, 0x00, 0x01, 0xFF, 0xFF, 0x07])
-        register = twseia.SmartApplication.create_read_version_cmd(service_id=0x00)
-        self.assertEqual(register, [0x06, 0x00, 0x02, 0xFF, 0xFF, 0x04])
-        register = twseia.SmartApplication.create_read_dev_type_cmd(service_id=0x00)
-        self.assertEqual(register, [0x06, 0x00, 0x04, 0xFF, 0xFF, 0x02])
-        register = twseia.SmartApplication.create_read_brand_cmd(service_id=0x00)
-        self.assertEqual(register, [0x06, 0x00, 0x05, 0xFF, 0xFF, 0x03])
-        register = twseia.SmartApplication.create_read_model_cmd(service_id=0x00)
-        self.assertEqual(register, [0x06, 0x00, 0x06, 0xFF, 0xFF, 0x00])
-        register = twseia.SmartApplication.create_read_services_cmd(service_id=0x00)
-        self.assertEqual(register, [0x06, 0x00, 0x07, 0xFF, 0xFF, 0x01])
-        register = twseia.SmartApplication.create_read_all_states_cmd(service_id=0x00)
-        self.assertEqual(register, [0x06, 0x00, 0x08, 0xFF, 0xFF, 0x0E])
-
-    def test_parsing_register_response(self):
-        test_pdu = []
-        response = twseia.SmartApplication.parsing_read_register_response(pdu=test_pdu)
-        self.assertTrue(isinstance(response, twseia.SAInfoRegisterPacket))
-        assert isinstance(response, twseia.SAInfoRegisterPacket)
-        self.assertTrue(response.type_id == 0x00)
-        self.assertTrue(response.class_id < 4)
-        self.assertTrue(response.service_id == 0x00)
-        self.assertTrue(isinstance(response.brand, str))
-        self.assertTrue(isinstance(response.model, str))
-        self.assertTrue(isinstance(response.major_ver, int))
-        self.assertTrue(isinstance(response.minor_ver, int))
-        self.assertTrue(isinstance(response.services, list))
-        self.assertTrue(len(response.services) > 0)
-
-        response = twseia.SmartApplication.parsing_read_class_id_response(pdu=test_pdu)
-        assert isinstance(response, twseia.SAInfoRegisterPacket)
-        self.assertTrue(response.type_id == 0x00)
-        self.assertTrue(response.service_id == 0x01)
-        self.assertTrue(response.class_id < 4)
-
-        response = twseia.SmartApplication.parsing_read_version_response(pdu=test_pdu)
-        assert isinstance(response, twseia.SAInfoRegisterPacket)
-        self.assertTrue(response.type_id == 0x00)
-        self.assertTrue(response.service_id == 0x02)
-        self.assertTrue(isinstance(response.major_ver, int))
-        self.assertTrue(isinstance(response.minor_ver, int))
-
-        response = twseia.SmartApplication.parsing_read_dev_type_response(pdu=test_pdu)
-        assert isinstance(response, twseia.SAInfoRegisterPacket)
-        self.assertTrue(response.type_id == 0x00)
-        self.assertTrue(response.service_id == 0x04)
-
-        response = twseia.SmartApplication.parsing_read_brand_response(pdu=test_pdu)
-        assert isinstance(response, twseia.SAInfoRegisterPacket)
-        self.assertTrue(response.type_id == 0x00)
-        self.assertTrue(response.service_id == 0x05)
-        self.assertTrue(isinstance(response.brand, str))
-
-        response = twseia.SmartApplication.parsing_read_model_response(pdu=test_pdu)
-        assert isinstance(response, twseia.SAInfoRegisterPacket)
-        self.assertTrue(response.type_id == 0x00)
-        self.assertTrue(response.service_id == 0x06)
-        self.assertTrue(isinstance(response.model, str))
-
-        response = twseia.SmartApplication.parsing_read_services_response(pdu=test_pdu)
-        assert isinstance(response, twseia.SAInfoRegisterPacket)
-        self.assertTrue(response.type_id == 0x00)
-        self.assertTrue(response.service_id == 0x07)
-        self.assertTrue(isinstance(response.services, list))
-        self.assertTrue(len(response.services) > 0)
-
-        response = twseia.SmartApplication.parsing_read_all_states_response(pdu=test_pdu)
-        assert isinstance(response, twseia.SAInfoRegisterPacket)
-        self.assertTrue(response.type_id == 0x00)
-        self.assertTrue(response.service_id == 0x08)
-        self.assertTrue(isinstance(response.states, dict))
-        self.assertTrue(len(response.states) > 0)
-
-    def test_create_read_state_cmd(self):
-        pass
-
-    def test_parsing_read_state_response(self):
-        pass
-
-    def test_create_write_state_cmd(self):
-        pass
-
-    def test_parsing_write_state_response(self):
-        pass
+# class TestSmartApplication(unittest.TestCase):
+#
+#     def test_create_register_cmd(self):
+#         register = twseia.SmartApplication.create_read_register_cmd(service_id=0x00)
+#         self.assertEqual(register, [0x06, 0x00, 0x00, 0xFF, 0xFF, 0x06])
+#         register = twseia.SmartApplication.create_read_class_id_cmd(service_id=0x00)
+#         self.assertEqual(register, [0x06, 0x00, 0x01, 0xFF, 0xFF, 0x07])
+#         register = twseia.SmartApplication.create_read_version_cmd(service_id=0x00)
+#         self.assertEqual(register, [0x06, 0x00, 0x02, 0xFF, 0xFF, 0x04])
+#         register = twseia.SmartApplication.create_read_dev_type_cmd(service_id=0x00)
+#         self.assertEqual(register, [0x06, 0x00, 0x04, 0xFF, 0xFF, 0x02])
+#         register = twseia.SmartApplication.create_read_brand_cmd(service_id=0x00)
+#         self.assertEqual(register, [0x06, 0x00, 0x05, 0xFF, 0xFF, 0x03])
+#         register = twseia.SmartApplication.create_read_model_cmd(service_id=0x00)
+#         self.assertEqual(register, [0x06, 0x00, 0x06, 0xFF, 0xFF, 0x00])
+#         register = twseia.SmartApplication.create_read_services_cmd(service_id=0x00)
+#         self.assertEqual(register, [0x06, 0x00, 0x07, 0xFF, 0xFF, 0x01])
+#         register = twseia.SmartApplication.create_read_all_states_cmd(service_id=0x00)
+#         self.assertEqual(register, [0x06, 0x00, 0x08, 0xFF, 0xFF, 0x0E])
+#
+#     def test_parsing_register_response(self):
+#         test_pdu = []
+#         response = twseia.SmartApplication.parsing_read_register_response(pdu=test_pdu)
+#         self.assertTrue(isinstance(response, twseia.SAInfoRegisterPacket))
+#         assert isinstance(response, twseia.SAInfoRegisterPacket)
+#         self.assertTrue(response.type_id == 0x00)
+#         self.assertTrue(response.class_id < 4)
+#         self.assertTrue(response.service_id == 0x00)
+#         self.assertTrue(isinstance(response.brand, str))
+#         self.assertTrue(isinstance(response.model, str))
+#         self.assertTrue(isinstance(response.major_ver, int))
+#         self.assertTrue(isinstance(response.minor_ver, int))
+#         self.assertTrue(isinstance(response.services, list))
+#         self.assertTrue(len(response.services) > 0)
+#
+#         response = twseia.SmartApplication.parsing_read_class_id_response(pdu=test_pdu)
+#         assert isinstance(response, twseia.SAInfoRegisterPacket)
+#         self.assertTrue(response.type_id == 0x00)
+#         self.assertTrue(response.service_id == 0x01)
+#         self.assertTrue(response.class_id < 4)
+#
+#         response = twseia.SmartApplication.parsing_read_version_response(pdu=test_pdu)
+#         assert isinstance(response, twseia.SAInfoRegisterPacket)
+#         self.assertTrue(response.type_id == 0x00)
+#         self.assertTrue(response.service_id == 0x02)
+#         self.assertTrue(isinstance(response.major_ver, int))
+#         self.assertTrue(isinstance(response.minor_ver, int))
+#
+#         response = twseia.SmartApplication.parsing_read_dev_type_response(pdu=test_pdu)
+#         assert isinstance(response, twseia.SAInfoRegisterPacket)
+#         self.assertTrue(response.type_id == 0x00)
+#         self.assertTrue(response.service_id == 0x04)
+#
+#         response = twseia.SmartApplication.parsing_read_brand_response(pdu=test_pdu)
+#         assert isinstance(response, twseia.SAInfoRegisterPacket)
+#         self.assertTrue(response.type_id == 0x00)
+#         self.assertTrue(response.service_id == 0x05)
+#         self.assertTrue(isinstance(response.brand, str))
+#
+#         response = twseia.SmartApplication.parsing_read_model_response(pdu=test_pdu)
+#         assert isinstance(response, twseia.SAInfoRegisterPacket)
+#         self.assertTrue(response.type_id == 0x00)
+#         self.assertTrue(response.service_id == 0x06)
+#         self.assertTrue(isinstance(response.model, str))
+#
+#         response = twseia.SmartApplication.parsing_read_services_response(pdu=test_pdu)
+#         assert isinstance(response, twseia.SAInfoRegisterPacket)
+#         self.assertTrue(response.type_id == 0x00)
+#         self.assertTrue(response.service_id == 0x07)
+#         self.assertTrue(isinstance(response.services, list))
+#         self.assertTrue(len(response.services) > 0)
+#
+#         response = twseia.SmartApplication.parsing_read_all_states_response(pdu=test_pdu)
+#         assert isinstance(response, twseia.SAInfoRegisterPacket)
+#         self.assertTrue(response.type_id == 0x00)
+#         self.assertTrue(response.service_id == 0x08)
+#         self.assertTrue(isinstance(response.states, dict))
+#         self.assertTrue(len(response.states) > 0)
+#
+#     def test_create_read_state_cmd(self):
+#         pass
+#
+#     def test_parsing_read_state_response(self):
+#         pass
+#
+#     def test_create_write_state_cmd(self):
+#         pass
+#
+#     def test_parsing_write_state_response(self):
+#         pass
 
 
 class TestAirConditioner(unittest.TestCase):
